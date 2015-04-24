97ad8a459a125b18a34264810e8b7a9cd8a3ae9c
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-04-24 20:39:22 +0200

    Added program to test the algorithm and a container sample data file

--- include/container.h
+++ include/container.h
@@ -1,14 +1,49 @@
 #ifndef __CONTAINER_H__
 #define __CONTAINER_H__
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
 struct Container {
 	int weight;
 	int value;
+
+	Container() {	
+		// Se inicializa a un valor determinado para que no haya basura dificil de depurar
+		this->weight = 0;
+		this->value = 0;
+	}
 
 	Container(int weight, int value){
 		this->weight = weight;
 		this->value = value;
 	}
 };
+
+istream& operator>>(istream &is, Container &container) {
+	is >> container.weight;
+	is >> container.value;
+
+	return is;
+}
+
+istream& operator>>(istream &is, vector<Container> &containers) {
+	while( !is.eof() ) {
+		Container new_container;
+		is >> new_container;
+		containers.push_back(new_container);
+	}
+
+	return is;
+}
+
+ostream& operator<<(ostream &os, const Container &container){
+	os << "W: " << container.weight << ", V: " << container.value;
+
+	return os;
+}
 
 #endif
--- src/algorithms/max_containers.cpp
+++ src/algorithms/max_containers.cpp
@@ -2,23 +2,22 @@
 #include <vector>
 #include <algorithm>
 
+using namespace std;
 
 bool weight_order(Container a, Container b){
 	return a.weight < b.weight;
 }
 
-vector<Container> loadMaxNumberOfContainers(vector<Container> containers, int max_total_weight){
-
+vector<Container> getContainersToLoad(vector<Container> containers, int max_total_weight){
 	std::sort(containers.begin(), containers.end(), weight_order);
 
-	int total_weight = 0,
-		current_container_index = 0;
-
+	int total_current_weight = 0;
 	vector<Container> result;
 
 	for(Container container : containers) {
 
-		if(total_weight + container.weight <= max_total_weight) {
+		if(total_current_weight + container.weight <= max_total_weight) {
+			total_current_weight += container.weight;
 			result.push_back(container);
 		} else {
 			return result;
